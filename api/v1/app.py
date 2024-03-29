7b77d62718582a6e2af68cc81e522b4a182672e4
Effah001 <effah210@gmail.com>
2024-03-29 19:37:42 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,6 @@
 """
 principal application
 """
-
 import os
 from flask import Flask
 from models import storage
@@ -10,7 +9,6 @@ from api.v1.views import app_views
 
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
 
@@ -21,9 +19,8 @@ def teardown_context(obj):
 
 
 if __name__ == '__main__':
-    # Get host and port from environment variables or use default values
-    host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', default=5000))
 
-    # Run the Flask app
+    host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', default=5000)
+
     app.run(host, int(port), threaded=True)
