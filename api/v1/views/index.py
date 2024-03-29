5561a1b7285459ab4cc11cbddecaaae64bbb3db0
Effah001 <effah210@gmail.com>
2024-03-29 21:31:56 +0000

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """
 endpoint(route) status
 """
+from models import storage
 from flask import Flask
 from flask import jsonify
 from api.v1.views import app_views
@@ -11,3 +12,15 @@ from api.v1.views import app_views
 def status():
     """Returns a JSON status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """
+    Retrieves the number of each objects by type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
