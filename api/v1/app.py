aa3f73f4fdf841479cb64e9f56d3a29e3525c5ed
Effah001 <effah210@gmail.com>
2024-03-29 18:07:51 +0000

    update

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+def teardown_storage(exception):
+    storage.close()
+
+@app.teardown_appcontext
+def teardown_context(exception):
+    teardown_storage(exception)
+
+if __name__ == '__main__':
+    # Get host and port from environment variables or use default values
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+
+    # Run the Flask app
+    app.run(host=host, port=port, threaded=True, debug=True)
