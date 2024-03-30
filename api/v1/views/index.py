433b6d79772ad47fc41c986361fdbb6371c773a4
Droid <44108804+Effah001@users.noreply.github.com>
2024-03-30 07:14:25 +0000

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,4 +10,4 @@ from api.v1.views import app_views
 @app_views.route('/status')
 def status():
     """Returns a JSON status"""
-    return jsonify{"status": "OK"}
+    return jsonify({"status": "OK"})
