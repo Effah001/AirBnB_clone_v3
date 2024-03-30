81c5029a2840a976675d139f332eb9730cb3d50d
Droid <44108804+Effah001@users.noreply.github.com>
2024-03-30 04:59:38 +0000

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,13 @@
+#!/usr/bin/python3
+"""
+principal application
+"""
+
 import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+
 
 app = Flask(__name__)
 
