c419b4c7c65285b643c2af1f5e0d8d1819854b56
Droid <44108804+Effah001@users.noreply.github.com>
2024-03-30 04:58:03 +0000

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,14 @@
+#!/usr/bin/python3
+"""
+endpoint(route) status
+"""
 from flask import Blueprint
+
 
 app_views = Blueprint('app_views', __name__)
 
 
 @app_views.route('/status')
 def status():
+    """Returns a JSON status"""
     return {"status": "OK"}
