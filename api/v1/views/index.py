2b712b6bf9a9f6aedc47d5d15d602f5b7f06588d
Droid <44108804+Effah001@users.noreply.github.com>
2024-03-30 05:20:51 +0000

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,13 +2,12 @@
 """
 endpoint(route) status
 """
-from flask import Blueprint
-
-
-app_views = Blueprint('app_views', __name__)
+from flask import Flask
+from flask import jsonify
+from api.v1.views import app_views
 
 
 @app_views.route('/status')
 def status():
     """Returns a JSON status"""
-    return {"status": "OK"}
+    return jsonify{"status": "OK"}
