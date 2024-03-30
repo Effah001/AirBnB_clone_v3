c7a68c533be1162b0c7c35f4f5f447e334c0d2bd
Droid <44108804+Effah001@users.noreply.github.com>
2024-03-30 04:50:58 +0000

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-
+"""Init file for views module"""
 from flask import Blueprint
 
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 
 # Wildcard import of everything from the index module
 from api.v1.views.index import *
