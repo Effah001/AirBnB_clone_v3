34477761161d2504e4912068ec94f8a6eb847961
Droid <44108804+Effah001@users.noreply.github.com>
2024-03-31 08:25:14 +0000

    Update cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -18,9 +18,9 @@ def get_cities(state_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
-def get_state(state_id):
+def get_city(city_id):
     """Retrieves a city"""
-    state = storage.get(City, city_id)
+    city = storage.get(City, city_id)
     if city:
         return jsonify(city.to_dict())
     else:
