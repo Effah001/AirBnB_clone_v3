1bfd513192e500541a0b7b71201e94f22af3feaa
Droid <44108804+Effah001@users.noreply.github.com>
2024-03-31 08:36:17 +0000

    Update cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -63,7 +63,7 @@ def update_state(city_id):
     if not data:
         return make_response(jsonify({"error": "Not a JSON"}), 400)
     for key, value in data.items():
-        if key not in ['id', 'state_created', 'created_at', 'updated_at']:
+        if key not in ['id', 'state_id', 'created_at', 'updated_at']:
             setattr(city, key, value)
     storage.save()
-    return jsonify(state.to_dict()), 200
+    return jsonify(state.to_dict())
