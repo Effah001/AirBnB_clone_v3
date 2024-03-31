2e2bdd69ae8c549dfa2cbc844202f86817d2b9fd
Droid <44108804+Effah001@users.noreply.github.com>
2024-03-31 13:05:57 +0000

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -10,7 +10,10 @@ from api.v1.views import app_views
 def get_all_states():
     """Retrieves the list of all State objects"""
     states = storage.all(State).values()
-    return jsonify([state.to_dict() for state in states])
+    all_state = []
+    for state in states:
+        all_state.append(state.to_dict())
+    return jsonify(all_state)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
